bb2a06a62d075f9099889fa763f5b3fd1c9e922b
Dominik Martin <dominik.martin91@gmail.com>
2016-12-18 13:15:56 +0100

    define annotation namespace by extension

--- SemanticAnnotator.hooks.php
+++ SemanticAnnotator.hooks.php
@@ -8,11 +8,40 @@
 
 class SemanticAnnotatorHooks {
 	
-	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) { 
+	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 		if($out->getTitle()->getNamespace() == 0) {		//0 is the Aricle's Namespace
 			$out->addModules( 'ext.annotator' );
 		}
 		return true;
 	}
+
+	public static function onCanonicalNamespaces( array &$namespaces ) {
+        global $wgNamespacesWithSubpages;
+
+        if ( !defined( 'NS_ANNOTATION' ) ) {
+            define( 'NS_ANNOTATION', 248 );
+            define( 'NS_ANNOTATION_TALK', 249 );
+        }
+
+        $namespaces[NS_ANNOTATION] = 'Annotation';
+        $namespaces[NS_ANNOTATION_TALK] = 'Annotation_talk';
+
+        $wgNamespacesWithSubpages[NS_ANNOTATION] = true;
+
+        return true;
+    }
+
+    public static function afterInit( ) {
+        global $smwgNamespacesWithSemanticLinks;
+
+        if ( !defined( 'NS_ANNOTATION' ) ) {
+            define( 'NS_ANNOTATION', 248 );
+            define( 'NS_ANNOTATION_TALK', 249 );
+        }
+
+        $smwgNamespacesWithSemanticLinks[NS_ANNOTATION] = true;
+
+        return true;
+    }
 
 }
