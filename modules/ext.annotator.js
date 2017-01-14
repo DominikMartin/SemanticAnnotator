645e16cb6070b81831ca73e5c34a0e3de52bfb95
Dominik Martin <dominik.martin91@gmail.com>
2017-01-14 12:39:53 +0100

    loading spinner on extension start

--- modules/ext.annotator.js
+++ modules/ext.annotator.js
@@ -13,6 +13,7 @@ var loaded = false;
 
 	// do if annotate button is clicked
 	$('#ca-annotate').click(function() {
+		$('body').append('<div class="annotator-loading"><div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div></div>');
 		loaded = !loaded;
 		
 		if(loaded){
